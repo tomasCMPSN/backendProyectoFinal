fed96e1be17941bc9df8113abfb55d0a1079cbaf
Tomas Campusano <tomascampusano.10@gmail.com>
2022-04-09 20:11:20 -0300

    Posible solucion 17

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,3 @@
 module.exports = {
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.resolve.alias.https = "https-browserify";
-      config.resolve.alias.http = "http-browserify";
-      return config;
-    },
-    
-};
-
-module.exports = {
-    resolve: {
-        fallback: {
-            assert: require.resolve('assert'),
-            crypto: require.resolve('crypto-browserify'),
-            http: require.resolve('stream-http'),
-            https: require.resolve('https-browserify'),
-            os: require.resolve('os-browserify/browser'),
-            stream: require.resolve('stream-browserify'),
-        },
-    },
-};
+    target: 'node',
+}
